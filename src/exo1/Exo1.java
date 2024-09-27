221e26335234497e55cc0736b149834616c711c0
yohanF1245 <yohan.ferdin@hotmail.fr>
2024-09-27 19:05:01 +0200

    feat(exo1): firt exercice

---
+++ src/exo1/Exo1.java
@@ -0,0 +1,10 @@
+package exo1;
+
+public class Exo1 {
+
+	public static void main(String[] args) {
+		ShowString ex1 = new ShowString();
+		System.out.println(ex1.returnShowString());
+	}
+
+}     
---
+++ src/exo1/ShowString.java
@@ -0,0 +1,13 @@
+package exo1;
+
+public class ShowString {
+	private String helloWorld = "Hello, World !";
+
+	public String returnShowString() {
+		
+		return helloWorld;
+	}
+	
+	
+
+}
