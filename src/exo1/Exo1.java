c61897af812aea81fb1102c8745d636936343576
yohanF1245 <yohan.ferdin@hotmail.fr>
2024-09-27 21:39:26 +0200

    refacto(exo1): add exercice menu select and put each exercice in split functions

--- src/exo1/Exo1.java
+++ src/exo1/Exo1.java
@@ -3,29 +3,95 @@ package exo1;
 import java.util.Scanner;
 
 public class Exo1 {
-
+	static boolean continueRun = true;
 	public static void main(String[] args) {
 		
-		/*//EXERCICE 1 – AFFICHAGE D’UNE CHAINE
+		
+		while(continueRun) {
+			menu();
+		}
+	}
+	public static void menu(){
+		System.out.println("EXERCICE 1 – AFFICHAGE D’UNE CHAINE");
+		System.out.println("EXERCICE 2 – UTILSIATION DE VARIABLES");
+		System.out.println("EXERCICE 3 – CALCUL ARITHMETIQUE");
+		System.out.println("EXERCICE 4 – SAISIE DE VALEUR");
+		System.out.println("EXERCICE 5 – TEST SIMPLE");
+		System.out.println("EXERCICE 6 – TEST SIMPLE");
+		System.out.println("EXERCICE 7 – TEST AVANCE");
+		
+		
+		Scanner inputChoice = new Scanner(System.in);
+		System.out.println("Selectionner le numero de l'exercice : ");
+		
+		int choice = inputChoice.nextInt();
+		switch (choice) {
+		case 1: {
+			exercice1();
+			break;
+		}
+		case 2: {
+			exercice2();
+			break;
+		}
+		case 3: {
+			exercice3();
+			break;
+		}
+		case 4: {
+			exercice4();
+			break;
+		}
+		case 5: {
+			exercice5();
+			break;
+		}
+		case 6: {
+			exercice6();
+			break;
+		}
+		case 7: {
+			exercice7();
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + choice);
+		}
+		Scanner inputContinue= new Scanner(System.in);
+		System.out.println("Stopper le programme y/n");
+		char continueProgram = inputContinue.next().charAt(0);
+		if(continueProgram == 'n') {
+			continueRun=(false);
+		}
+		}
+	
+	public static void exercice1() {
+		//EXERCICE 1 – AFFICHAGE D’UNE CHAINE
 		//Afficher le texte Hello World dans la console de sortie
 		
 		ShowString ex1 = new ShowString();
 		System.out.println(ex1.getHelloWorld());
-		
+	}
+	
+	public static void exercice2() {
 		// EXERCICE 2 – UTILSIATION DE VARIABLES
 		// Reprendre l’exercice n°1 en utilisant une variable
-		
+				
 		ShowString ex2 = new ShowString();
 		ex2.setHelloWorld("Goodbye, World!");
-		System.out.println(ex2.getHelloWorld());
-		
+		System.out.println(ex2.getHelloWorld());		
+	}
+	
+	public static void exercice3() {
 		//EXERCICE 3 – CALCUL ARITHMETIQUE
 		//Afficher le périmètre d’un cercle dont le rayon est une variable
 		
 		CirclePerimeter ex3 = new CirclePerimeter();
 		ex3.setRayon(4);
 		System.out.println("Le périmètre d'un cercle de rayon "+ex3.getRayon()+" est de "+ex3.calculatePerimeter()+" centimètres");
-		
+	}
+	
+	public static void exercice4() {
 		//EXERCICE 4 – SAISIE DE VALEUR
 		//Afficher le périmètre d’un cercle dont le rayon est demandé à l’utilisateur
 		
@@ -33,11 +99,9 @@ public class Exo1 {
 	
 		Scanner inputRayon = new Scanner(System.in);
 		System.out.println("Indiquez le rayon : ");
-		
-		float rayon = inputRayon.nextFloat();
-		ex4.setRayon(rayon);
-		System.out.println("Le périmètre d'un cercle de rayon "+ex4.getRayon()+" est de "+ex4.calculatePerimeter()+" centimètres");
-		
+	}
+	
+	public static void exercice5() {
 		//EXERCICE 5 – TEST SIMPLE
 		//A partir d’une valeur saisie, vérifier si l’utilisateur est majeur ou mineur
 		
@@ -49,7 +113,9 @@ public class Exo1 {
 		int age = inputAge.nextInt();
 		ex5.setAge(age);
 		System.out.println("Vous etes : "+ex5.majorityTest());
-		
+	}
+	
+	public static void exercice6() {
 		//EXERCICE 6 – TEST SIMPLE
 		//A partir d’une valeur saisie, indiquer si le nombre saisi est pair ou impair
 		
@@ -60,8 +126,10 @@ public class Exo1 {
 		
 		int num = inputNum.nextInt();
 		ex6.setNumToTest(num);
-		System.out.println("Le nombre "+num+" est : "+ex6.evenOrOdd());*/
-		
+		System.out.println("Le nombre "+num+" est : "+ex6.evenOrOdd());
+	}
+	
+	public static void exercice7() {
 		//EXERCICE 7 – TEST AVANCE
 		//Vérifier si une année est bissextile.
 		//Pour rappel, une année bissextile est une année multiple de 4, mais pas multiple de 100 ou alors multiple de
@@ -69,9 +137,11 @@ public class Exo1 {
 		
 		LeapYear ex7 = new LeapYear();
 		
-		Scanner inputYear = new 
+		Scanner inputYear = new Scanner(System.in);
+		System.out.println("Entrez une année");
 		
-		
+		int year = inputYear.nextInt();
+		ex7.setYear(year);
+		System.out.println("L'année "+year+" "+ex7.LeapYear());		
 	}
-
 }     
--- src/exo1/LeapYear.java
+++ src/exo1/LeapYear.java
@@ -1,5 +1,21 @@
 package exo1;
 
 public class LeapYear {
+	private int year = 0;
 
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public String LeapYear() {
+		if((year%4==0 && year%100!=0 )|| year%400==0) {
+			return "est bissextile";
+		}else {
+			return "n'est pas bissextile";
+		}
+	}
 }
